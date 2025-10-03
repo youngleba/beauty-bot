8426361d8fb47213a7619300a17e9d705f671a36
youngleba <youngleba7@gmail.com>
2025-10-03 14:47:40 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const TelegramBot = require("node-telegram-bot-api");
+
+// сюда вставь токен, который тебе дал BotFather
+const token = "8337610302:AAFZkf26fMgxjVMdnnXI1XWr389RbSj6BTk";
+
+const bot = new TelegramBot(token, { polling: true });
+
+// команда /start
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Привет! Я бот для записи 😊");
+});
+
+// пример кнопок
+bot.on("message", (msg) => {
+  if (msg.text === "Записаться") {
+    bot.sendMessage(msg.chat.id, "Выберите время:", {
+      reply_markup: {
+        keyboard: [["10:00", "12:00"], ["14:00", "16:00"]],
+        resize_keyboard: true,
+        one_time_keyboard: true,
+      },
+    });
+  }
+});
